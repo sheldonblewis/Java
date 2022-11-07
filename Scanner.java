e35fcc36e1a6c24f6bfe3265708482766964b197
Sheldon Lewis <117256319+sheldonblewis@users.noreply.github.com>
2022-11-07 13:15:51 -0500

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -21,9 +21,10 @@ public class Scanner
 		
 		try
 		{
-			this.plane = ImageIO.read(new File("C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\plane.jpg"));
-			this.truck = ImageIO.read(new File("C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\truck.png"));
-			this.question = ImageIO.read(new File("C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\question.jpg"));
+			//Replace the following files with paths to the downloaded plane.jpg, truck.png, and question.jpg files on your computer
+			this.plane = ImageIO.read(new File(/*"C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\plane.jpg"*/));
+			this.truck = ImageIO.read(new File(/*"C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\truck.png"*/));
+			this.question = ImageIO.read(new File(/*"C:\\Users\\lewisv\\eclipse-workspace\\Parcel Distribution Center\\src\\ParcelDistributionCenter\\question.jpg"*/));
 		}
 		catch (IOException e)
 		{
